c057f21f3a574f02fd81985bb8ee5368f8056120
slawomir.stankiewicz <lAndraw4$5>
2024-04-11 00:10:56 +0200

    S8 done + refactor

--- src/e2e/pages/interactive-pages/puppyAdoptionConfirmation.ts
+++ src/e2e/pages/interactive-pages/puppyAdoptionConfirmation.ts
@@ -1,13 +1,15 @@
 import { InteractivePage } from '../interactivePage.js';
 import { Selector } from 'webdriverio';
 
-export default new class AdoptionConfirmation extends InteractivePage {
+export default new class PuppyAdoptionConfirmation extends InteractivePage {
     protected readonly pageName: string = 'adoption confirmation page';
     protected readonly locatorsMap: Map<string, Selector> = new Map<string, Selector>([
         [this.pageName,'//*[contains(text(),"Your Litter")]'],
         ['Change Your Mind button','[value="Change your mind"]'],
         ['Adopt Another Puppy button', '[value="Adopt Another Puppy"]'],
         ['Complete the Adoption button', '[value="Complete the Adoption"]'],
-        ['adoption summary puppy names', 'tr:not(.total_line) td:nth-child(2) h2']
+        ['adoption summary puppy names', 'tr:not(.total_line) td:nth-child(2) h2'],
+        ['travel carrier checkbox','#carrier'],
+        ['total adoption fee', '.total_cell h2']
     ]);
 }
--- src/e2e/pages/interactive-pages/puppyListing.ts
+++ src/e2e/pages/interactive-pages/puppyListing.ts
@@ -3,29 +3,30 @@ import { Selector } from "webdriverio"
 import { Utils } from '../../../utilities/utils.js';
 import { ElementActions } from '../../../utilities/elementActions.js';
 
-export default new class PuppyList extends InteractivePage {
+export default new class PuppyListing extends InteractivePage {
+
+    private static puppyNames = ['Hanna', 'Twinkie', 'Spud', 'Maggie Mae', 'Brook'];
+
+    private static getPuppyDetails(puppyName: string): string[] {
+        const selectorName = `view details button for the puppy called ${puppyName}`;
+        const selector = `//*[contains(text(),"${puppyName}")]/parent::*//following-sibling::*[@class="view"]`;
+        return [selectorName, selector];
+    }
 
     protected readonly pageName: string = "puppy list page";
     protected readonly locatorsMap: Map<string, Selector> = new Map([
         [this.pageName,'//h2[contains(text(), "Puppy List")]'],
         ['any view details button', '[value="View Details"]'],
-        ['view details button for the puppy called Hanna', PuppyList.geViewDetailsSelectorFor('Hanna')],
-        ['view details button for the puppy called Twinkie', PuppyList.geViewDetailsSelectorFor('Twinkie')],
-        ['view details button for the puppy called Spud', PuppyList.geViewDetailsSelectorFor('Spud')],
-        ['view details button for the puppy called Maggie Mae', PuppyList.geViewDetailsSelectorFor('Maggie Mae')],
         ['the puppy named Maggie May', '//*[contains(text(),"Maggie Mae")]'],
         ['current page number', '.current'],
         ['puppies names on list', '.name'],
         ['second page pagination number','[aria-label="Page 2"]'],
         ['next page pagination button','.next_page:not(.disabled)'],
-        ['confirmation info', '#notice']
+        ['confirmation info', '#notice'],
+        ...PuppyListing.puppyNames.map(PuppyListing.getPuppyDetails)
     ]);
 
-    private static geViewDetailsSelectorFor(puppyName: string): string{
-        return `//*[contains(text(),"${puppyName}")]/parent::*//following-sibling::*[@class="view"]`
-    }
-
-    public async navigateThroughPaginationUntilFindingText(text, maxPages: number){
+    public async navigateThroughPaginationUntilFindingText(text: string, maxPages: number){
         for(let i = 0; i < maxPages; i++){
             if((await Utils.getConcatenatedText(await ElementActions.getElements("puppies names on list"))).includes(text)){
                 return true;
